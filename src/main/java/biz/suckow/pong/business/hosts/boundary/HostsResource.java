23e279294d45c230c86f29072f47366fb710f31e
Tobias <tobias@suckow.biz>
2015-01-06 21:19:48 +0100

    Implemented basic access. On the way introducing security.

--- src/main/java/biz/suckow/pong/business/hosts/boundary/HostsResource.java
+++ src/main/java/biz/suckow/pong/business/hosts/boundary/HostsResource.java
@@ -1,49 +1,78 @@
 package biz.suckow.pong.business.hosts.boundary;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
-import biz.suckow.pong.business.hosts.control.IPStore;
-import biz.suckow.pong.business.hosts.entity.RemoteHost;
+import biz.suckow.pong.business.hosts.control.HostStore;
+import biz.suckow.pong.business.hosts.entity.Host;
 
 @Singleton
-@Path("hosts")
+@Path(HostsResource.PATH_BASE)
 public class HostsResource {
+    public static final String PATH_BASE = "hosts";
+    public static final String RELPATH_LIST_ALL = "all";
+
     @Inject
-    private IPStore store;
+    private HostStore store;
 
     @GET
     public Response index() {
-	return Response.ok("add(/{hostname}/{ip}/{service}/{port}) or get (/get/{hostname}).")
-		.entity(this.getClass().getSimpleName()).build();
+	return Response.ok().entity(this.getClass().getSimpleName()).build();
     }
 
     @POST
-    @Path("/add/{hostname}/{ip}/{service}/{port}")
-    public Response add(@PathParam("hostname") final String hostname, @PathParam("ip") final String ip,
-	    @PathParam("service") final String service, @PathParam("port") final int port) {
-	final RemoteHost remoteHost = new RemoteHost().setHostname(hostname).setIp(ip).setPort(port)
-		.setService(service);
-	this.store.save(remoteHost);
-	return Response.ok().build();
+    @Path("{hostname}/{ip}/{port}/{service}")
+    public Response addHost(@NotNull @Size(min = 3) @PathParam("hostname") final String hostname,
+	    @NotNull @Size(min = 7) @PathParam("ip") final String ip,
+	    @NotNull @Size(min = 3) @PathParam("service") final String service,
+	    @NotNull @DecimalMin(value = "1") @PathParam("port") final int port,
+	    @HeaderParam("X-TOKEN") final String token, @Context final UriInfo info) {
+	final Host remoteHost = new Host().setHostname(hostname).setIp(ip).setPort(port).setService(service);
+	this.store.add(remoteHost);
+
+	final String path = new StringBuilder("/").append(PATH_BASE).append("/").append(remoteHost.getHostname())
+		.toString();
+	final URI uri = info.getBaseUriBuilder().path(path).build();
+	return Response.created(uri).build();
     }
 
     @GET
-    @Path("/get/{hostname}")
-    public Response get(@PathParam("hostname") final String hostname) {
+    @Path("{hostname}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getHost(@HeaderParam("X-TOKEN") final String token, @PathParam("hostname") final String hostname)
+	    throws URISyntaxException {
 	Response response = Response.status(Status.NOT_FOUND).build();
-	final Optional<RemoteHost> possibleHost = this.store.get(hostname);
+	final Optional<Host> possibleHost = this.store.get(hostname);
 	if (possibleHost.isPresent()) {
 	    response = Response.ok(possibleHost.get()).build();
 	}
 	return response;
+    }
+
+    @GET
+    @Path(RELPATH_LIST_ALL)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAllHosts(@HeaderParam("X-TOKEN") final String token) {
+	final List<Host> hosts = this.store.getAll();
+	return Response.ok(hosts).build();
     }
 }
--- src/test/java/biz/suckow/pong/business/hosts/boundary/HostsResourceIT.java
+++ src/test/java/biz/suckow/pong/business/hosts/boundary/HostsResourceIT.java
@@ -1,9 +1,15 @@
 package biz.suckow.pong.business.hosts.boundary;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OverProtocol;
@@ -14,9 +20,13 @@ import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HostsResourceIT extends Arquillian {
+import biz.suckow.pong.business.hosts.entity.Host;
 
-    @Deployment(testable=false)
+public class HostsResourceIT extends Arquillian {
+    private final Client client = ClientBuilder.newClient();
+    private static final String BASE = "http://localhost:8080/pong-0.0.1-SNAPSHOT/resources/hosts";
+
+    @Deployment(testable = false)
     @OverProtocol("Servlet 3.0")
     public static WebArchive deploy() {
 	return ShrinkWrap.create(MavenImporter.class).loadPomFromFile("pom.xml").importBuildOutput()
@@ -24,24 +34,41 @@ public class HostsResourceIT extends Arquillian {
     }
 
     @Test
-    public void verifyRESTAPIAvailable() {
-	final Client client = ClientBuilder.newClient();
-	final WebTarget target = client.target("http://localhost:8080/pong-0.0.1-SNAPSHOT/resources/hosts/");
-
-	final Response response = target.request().get();
+    public void verifyEndpointIsAvailable() {
+	final Response response = this.client.target(BASE).request().get();
 	Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
     }
 
     @Test
-    public void verifyAddedRemoteHostCanBeListed() {
-	final Client client = ClientBuilder.newClient();
-	WebTarget target = client.target("http://localhost:8080/pong-0.0.1-SNAPSHOT/resources/hosts/add/duke/127.0.0.1/ssh/22");
-	Response response = target.request().post(null);
-	Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+    public void verifyAddingAHostReturnsCreated() {
+	final Response response = this.client.target(BASE + "/duke/127.0.0.1/22/ssh").request().post(null);
+	assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+	assertThat(response.getLocation().toString()).isEqualTo(BASE + "/duke");
+    }
 
-	target = client.target("http://localhost:8080/pong-0.0.1-SNAPSHOT/resources/hosts/get/duke");
-	response = target.request().get();
-	Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-	System.out.println(".----------------------------------->"+response.getEntity());
+    @Test
+    public void verifyRoundtripOfAdditionAndRetrivalWorks() {
+	final Response response = this.client.target(BASE + "/dutchess/127.0.0.1/22/ssh").request().post(null);
+	assertThat(response.getStatus()).isEqualTo(Status.CREATED.getStatusCode());
+
+	final Host actualHost = this.client.target(BASE + "/dutchess").request(MediaType.APPLICATION_JSON)
+		.get(Host.class);
+	assertThat(actualHost).isNotNull();
+	assertThat(actualHost.getHostname()).isEqualTo("dutchess");
+	assertThat(actualHost.getIp()).isEqualTo("127.0.0.1");
+	assertThat(actualHost.getService()).isEqualTo("ssh");
+    }
+
+    @Test
+    public void list() {
+	this.client.target(BASE + "/duke1/127.0.0.1/22/ssh").request().post(null);
+	this.client.target(BASE + "/duke2/127.0.0.1/22/ssh").request().post(null);
+
+	final List<Host> hosts = this.client.target(BASE + "/" + HostsResource.RELPATH_LIST_ALL).request()
+		.get(new GenericType<List<Host>>() {
+		});
+	assertThat(hosts.size()).isGreaterThanOrEqualTo(2);
+	assertThat(hosts).contains(new Host().setHostname("duke1"));
+	assertThat(hosts).contains(new Host().setHostname("duke2"));
     }
 }
